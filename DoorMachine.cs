eb6a0e4ae27b71655cccddfbeb3a930bb1f1ad93
Mri-cia <rawikara8c@gmail.com>
2026-03-15 19:09:13 +0700

    menambahkan class DoorMachine

---
+++ DoorMachine.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Door
+{
+    internal enum State { TERKUNCI, TERBUKA };
+
+    internal class DoorMachine
+    {
+        private State _currentState = State.TERKUNCI;
+        public State CurrentState
+        {
+            get => _currentState;
+            set => _currentState = value;
+        }
+
+        public DoorMachine() { }
+
+        public void EnterDoor()
+        {
+            switch (CurrentState)
+            {
+                case State.TERKUNCI:
+                    Console.WriteLine("Pintu terkunci");
+                    break;
+                case State.TERBUKA:
+                    Console.WriteLine("Pintu tidak terkunci");
+                    break; 
+                default:
+                    break;
+            }
+        }
+
+        public void Unlock()
+        {
+            if (CurrentState == State.TERKUNCI) CurrentState = State.TERBUKA;
+            Console.WriteLine("Membuka...");
+        }
+
+        public void Lock()
+        {
+            if (CurrentState == State.TERBUKA) CurrentState = State.TERKUNCI;
+            Console.WriteLine("Mengunci...");
+        }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,19 @@
-﻿
+﻿using Door;
+
 var Pos = new KodePos();
 
 Pos.GetKodePos();
+
+Console.WriteLine();
+
+Door.DoorMachine pintu1 = new();
+
+pintu1.EnterDoor();
+pintu1.Lock();
+pintu1.EnterDoor();
+pintu1.Unlock();
+pintu1.EnterDoor();
+pintu1.Unlock();
+pintu1.EnterDoor();
+pintu1.Lock();
+pintu1.EnterDoor();
